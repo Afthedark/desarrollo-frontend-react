739cd77c1c7c168eafad0d9fe0b79f1022705d84
Luis Gonzalo <afthedark@hotmail.com>
2024-08-25 21:21:13 -0400

    delete hash

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createHashRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 import Default from "../screens/Default";
 import Product from "../screens/Product";
@@ -9,8 +9,7 @@ import App from "../App";
 
 const basename = process.env.NODE_ENV === 'production' ? '/desarrollo-frontend-react' : '/';
 
-//const routes = createBrowserRouter([
-const routes = createHashRouter([
+const routes = createBrowserRouter([
     {
         path: '/',
         element: <App />,
